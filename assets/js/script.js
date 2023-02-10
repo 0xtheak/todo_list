306d5ea66b48834c4e1fa956af514845670fd537
theak <theak@theak.theak>
2023-02-10 11:44:06 +0530

    correct the errors and typos

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,14 @@
 let todoInput = document.getElementById("todo-input");
 let plusBtn = document.getElementById("plus-btn");
 let todoList = document.getElementById("todo-list");
+let count = document.getElementById("count");
 let itemsList = ["Learn How to type", "Do exercise"];
 
 // print todo list
 function printItemsList() {
 	// clear previous items list 
 	todoList.innerHTML = '';
+	count.innerText = itemsList.length;
 	for (let i of itemsList) {
 
 		let li = document.createElement("li");
@@ -46,17 +48,24 @@ todoInput.addEventListener("input", toggleBtnVisibility);
 
 todoInput.addEventListener("keypress", (e) => {
 	if(e.key === 'Enter'){
-		let item = todoInput.value;
-		itemsList.push(item);
-		printItemsList();
-		console.log(itemsList);
+		if(todoInput.value!= ""){
+			let item = todoInput.value;
+			itemsList.push(item);
+			todoInput.value="";
+			printItemsList();
+			console.log(itemsList);
+		}
+		
 	}
 });
 plusBtn.addEventListener('click', (e)=> {
-	let item = todoInput.value;
-		itemsList.push(item);
-		printItemsList();
-		console.log(e);
+	if(todoInput.value!= ""){
+			let item = todoInput.value;
+			itemsList.push(item);
+			todoInput.value="";
+			printItemsList();
+			console.log(itemsList);
+		}
 })
 
 printItemsList();
